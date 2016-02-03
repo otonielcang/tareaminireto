8eedd2eb7912ba01e1b602e4a32dd03b7a93c0ae
Otoniel Can Gomez <nancychgo@gmail.com>
2016-02-03 14:04:16 -0600

    Juego de memoria

--- tarea/main.swift
+++ tarea/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+var numero = 0...100
+
+
+for contador in numero{
+    let bingo = contador % 5
+    let par = contador % 2
+    if bingo == 0 && contador > 0{
+        print("\n\(contador)\t Bingo!!!")
+    }
+    if par == 0 && contador > 0{
+        print("\n\(contador)\t Par")
+    }
+    if par == 1 {
+        print("\n\(contador)\t Impar")
+    }
+    if contador >= 30 && contador <= 40 {
+        print("\n\(contador)\t Viva Swift!!!")
+    }
+}
+
+
+
 

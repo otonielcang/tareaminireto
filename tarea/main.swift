ebeba3ce864191e805aee5fa4f0b7eaa5471d4aa
Otoniel Can Gomez <nancychgo@gmail.com>
2016-02-03 13:52:28 -0600

    Initial Commit

---
+++ tarea/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tarea
+//
+//  Created by Otoniel Can on 03/02/16.
+//  Copyright © 2016 Otoniel Can. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
